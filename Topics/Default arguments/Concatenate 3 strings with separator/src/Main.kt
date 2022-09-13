3ee26ca864c791a71f9fe58c3c32eba4a29843e8
proroker1 <proroker1@gmail.com>
2022-09-13 07:31:42 +0300

    Test
    Test

---
+++ Topics/Default arguments/Concatenate 3 strings with separator/src/Main.kt
@@ -0,0 +1,4 @@
+// complete this function
+fun concatenate(fWord: String, sWord: String, tWord: String, separator: String = " "): String {
+    return "$fWord$separator$sWord$separator$tWord"
+}
---
+++ Topics/Default arguments/Print Username/src/Main.kt
@@ -0,0 +1,5 @@
+// complete this function
+fun greetings(name: String = "secret user"): String {
+    return "Hello, $name!"
+
+}
